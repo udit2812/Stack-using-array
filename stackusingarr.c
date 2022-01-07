919db2810e12991f7560e1e2dc331665a4eb88fd
Udit Dhawan <64426117+udit2812@users.noreply.github.com>
2022-01-07 17:41:28 +0530

    Add files via upload

---
+++ stackusingarr.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct stack{
+    int size;
+    int top;
+    int *S;
+};
+
+void create(struct stack *st){
+    printf("Enter the size of array");
+    scanf("%d",&st->size);
+    st->top==-1;
+    st->S=(int*)malloc(st->size*sizeof(int));
+}
+
+void display(struct stack st){
+    int i;
+    for(i=st.top;i>=0;i--){
+        printf("%d ", st.S[i]);
+    }
+    printf("/n");
+}
+
+void push(struct stack *st,int x){
+    if(st->top==st->size-1)
+    printf("Stack overflow");
+    else{
+        st->top++;
+        st->S[st->top]=x;
+    }
+}
+
+int pop(struct stack *st)
+{
+    int x=-1;
+    if(st->top==-1)
+printf("Stack underflow");
+else{
+    x=st->S[st->top--];
+}
+return x;
+}
+
+int peek(strcut stack st,int index){
+    int x=-1;
+    if(st.top-index+1<0){
+        printf("Invalid");
+    }
+    else{
+        x=st.S[st.top-index+1];
+           }
+           return x;
+}
+
+int isEmpty(struct stack st){
+    if(st.top==-1)
+    return 1;
+    return 0;
+}
+
+int isFull(struct stack st){
+    return st.top==st.size-1
+}
+
+int stackTop(struct stack st){
+    if(!isEmpty(st))
+    return st.S[st.top];
+    return -1;
+}
+
+int main(){
+struct stack st;
+create(&st);
+push(&st,10);
+push(&st,20);
+push(&st,30);
+
+
+display(st);
+    return 0;
+}
